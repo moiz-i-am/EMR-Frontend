d4c898e06641fccf2305d05cc1075d8e4408e54d
Muhammad Moiz Tahir <moizchaudhary35@gmail.com>
2020-12-17 00:56:39 +0500

    download doctors prescription as pdf

--- src/components/dashboardContainers/prescriptions/PrescriptionCards/PatientCard.js
+++ src/components/dashboardContainers/prescriptions/PrescriptionCards/PatientCard.js
@@ -1,9 +1,65 @@
 import React, { Component } from "react";
-import { Button, Modal } from "semantic-ui-react";
+import { Button, Modal, Divider } from "semantic-ui-react";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 import logo from "../../../../assets/L.png";
 
 class PatientCard extends Component {
+  // printDocument() {
+  //   const input = document.getElementById("divToPrint");
+  //   html2canvas(input).then(canvas => {
+  //     const imgData = canvas.toDataURL("image/png");
+
+  //     // origin
+  //     const pdf = new jsPDF("p", "pt", "a4");
+  //     pdf.addImage(imgData, "JPEG", 0, 0);
+  //     pdf.save("download.pdf");
+  //   });
+  // }
+
+  printDocument() {
+    const input = document.getElementById("divToPrint");
+    const pdf = new jsPDF("p", "pt", "a4");
+    const inputHeight = input.clientHeight;
+    const inputWidth = input.clientWidth;
+    let height = pdf.internal.pageSize.height;
+    let pageHeightInPixels = inputHeight;
+    let pages = pageHeightInPixels / height;
+
+    console.log(pageHeightInPixels + "," + height);
+
+    const roundOff = Number(
+      pages
+        .toString()
+        .split(".")[1]
+        .substring(0, 1)
+    );
+    const pageNo = roundOff > 0 ? pages + 1 : pages;
+    let pageCount = pages < 1 ? 1 : Math.trunc(pageNo);
+
+    html2canvas(input).then(canvas => {
+      const imgData = canvas.toDataURL("image/png");
+
+      // origin
+      for (let i = 1; i <= pageCount; i++) {
+        let pdfStartingHeight = height * (i - 1);
+        pdf.addImage(
+          imgData,
+          "PNG",
+          0,
+          -pdfStartingHeight,
+          inputWidth * 0.8,
+          inputHeight * 0.8
+        );
+        if (i < pageCount) {
+          pdf.addPage();
+        }
+      }
+      pdf.save(`prescription-${new Date()}.pdf`);
+    });
+  }
+
   render() {
     return (
       <div>
@@ -33,7 +89,7 @@ class PatientCard extends Component {
                 </p>
 
                 <Modal
-                  style={{ backgroundColor: "#faed86" }}
+                  style={{ backgroundColor: "#fff" }}
                   size={"small"}
                   trigger={
                     <Button
@@ -43,112 +99,133 @@ class PatientCard extends Component {
                     </Button>
                   }
                 >
-                  <Modal.Header
-                    style={{
-                      backgroundColor: "#fff"
-                    }}
-                  >
-                    <div
+                  <div id="divToPrint" className="divToPrint">
+                    <Modal.Header
                       style={{
-                        display: "flex",
-                        justifyContent: "space-around"
-                      }}
-                    >
-                      <div style={{ width: "100%" }}>
-                        <img
-                          style={{ width: "50%" }}
-                          src={logo}
-                          alt="Company Logo"
-                        />
-                      </div>
-                      <div style={{ width: "50%" }}>
-                        <p style={{ fontSize: "14px" }}>
-                          phone: <span>0334-5959007</span>
-                        </p>
-                        <p style={{ fontSize: "14px" }}>
-                          email: <span>health-e@gmail.com</span>
-                        </p>
-                      </div>
-                    </div>
-                  </Modal.Header>
-                  <Modal.Description>
-                    <div
-                      style={{
-                        width: "100%",
-                        backgroundColor: "#fff",
-                        borderBottom: "1px solid black"
+                        backgroundColor: "#fff"
                       }}
                     >
                       <div
                         style={{
                           display: "flex",
-                          justifyContent: "space-between",
-                          padding: "10px"
+                          justifyContent: "space-around"
                         }}
                       >
-                        <div>
-                          <div style={{ padding: "5px" }}>
-                            <span
-                              style={{ fontWeight: "bold", fontSize: "20px" }}
-                            >
-                              {this.props.doctorName}
-                            </span>
-                          </div>
-                          <p style={{ margin: 0, textAlignLast: "right" }}>
-                            (Skin Specialist)
+                        <div style={{ width: "100%" }}>
+                          <img
+                            style={{ width: "50%" }}
+                            src={logo}
+                            alt="Company Logo"
+                          />
+                        </div>
+                        <div style={{ width: "50%" }}>
+                          <p style={{ fontSize: "14px" }}>
+                            phone: <span>0334-5959007</span>
                           </p>
-                          <p style={{ margin: 0, textAlignLast: "right" }}>
-                            POF Hospital, Wah Cantt
+                          <p style={{ fontSize: "14px" }}>
+                            email: <span>health-e@gmail.com</span>
                           </p>
                         </div>
-                        <div>{new Date(this.props.date).toDateString()}</div>
                       </div>
-                    </div>
-                  </Modal.Description>
-                  <Modal.Description>
-                    <div
-                      style={{
-                        width: "100%",
-                        backgroundColor: "#fff",
-                        paddingTop: "10px",
-                        paddingBottom: "10px",
-                        paddingLeft: "10px"
-                      }}
-                    >
+                    </Modal.Header>
+                    <Divider style={{ margin: 0 }} />
+                    <Modal.Description>
                       <div
                         style={{
-                          textAlign: "left",
-                          width: "50%",
-                          border: "1px solid #ccc",
-                          borderRadius: 10
+                          width: "100%",
+                          backgroundColor: "#fff"
                         }}
                       >
-                        <div style={{ padding: "5px" }}>
-                          <span style={{ fontWeight: "bold" }}>
-                            Patient Name:{" "}
-                          </span>
-                          <span style={{ fontWeight: "bold" }}>
-                            {this.props.patientName}
-                          </span>
-                        </div>
-
-                        <div style={{ padding: "5px" }}>
-                          <span>DOB: </span>
-                          <span>Nov 25 1998</span>
-                        </div>
-                        <div style={{ padding: "5px" }}>
-                          <span>Date: </span>
-                          <span>
-                            {new Date(this.props.date).toDateString()}
-                          </span>
+                        <div
+                          style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            padding: "10px"
+                          }}
+                        >
+                          <div>
+                            <div style={{ padding: "5px" }}>
+                              <span
+                                style={{ fontWeight: "bold", fontSize: "20px" }}
+                              >
+                                {this.props.doctorName}
+                              </span>
+                            </div>
+                            <p style={{ margin: 0, textAlignLast: "right" }}>
+                              (Skin Specialist)
+                            </p>
+                            <p style={{ margin: 0, textAlignLast: "right" }}>
+                              POF Hospital, Wah Cantt
+                            </p>
+                          </div>
+                          <div>{new Date(this.props.date).toDateString()}</div>
                         </div>
                       </div>
-                    </div>
-                  </Modal.Description>
+                    </Modal.Description>
+                    <Divider style={{ margin: 0 }} />
+                    <Modal.Description>
+                      <div
+                        style={{
+                          width: "100%",
+                          backgroundColor: "#fff",
+                          paddingTop: "10px",
+                          paddingBottom: "10px",
+                          paddingLeft: "10px"
+                        }}
+                      >
+                        <div
+                          style={{
+                            textAlign: "left",
+                            width: "50%",
+                            border: "1px solid #ccc",
+                            borderRadius: 10
+                          }}
+                        >
+                          <div style={{ padding: "5px" }}>
+                            <span style={{ fontWeight: "bold" }}>
+                              Patient Name:{" "}
+                            </span>
+                            <span style={{ fontWeight: "bold" }}>
+                              {this.props.patientName}
+                            </span>
+                          </div>
+
+                          <div style={{ padding: "5px" }}>
+                            <span>DOB: </span>
+                            <span>Nov 25 1998</span>
+                          </div>
+                          <div style={{ padding: "5px" }}>
+                            <span>Date: </span>
+                            <span>
+                              {new Date(this.props.date).toDateString()}
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    </Modal.Description>
+                    <Divider style={{ margin: 0 }} />
+                    <Modal.Description>
+                      <p
+                        className="notes"
+                        style={{
+                          whiteSpace: "pre-wrap",
+                          color: "black"
+                        }}
+                      >
+                        {this.props.prescriptionText}
+                      </p>
+                    </Modal.Description>
+                  </div>
+
                   <Modal.Description>
-                    <textarea className="notes" readOnly>
-                      {this.props.prescriptionText}
-                    </textarea>
+                    <div style={{ textAlign: "right", padding: "5px" }}>
+                      <Button
+                        style={{ backgroundColor: "#9458AE", color: "#fff" }}
+                        onClick={this.printDocument}
+                      >
+                        Download
+                      </Button>
+                    </div>
                   </Modal.Description>
                 </Modal>
               </div>
